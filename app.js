2702805330270b1ac11dcd8dd82703ef4a5a0a85
D4rK14 <mastahoscuro@gmail.com>
2025-08-29 23:52:20 -0400

    handlebars establecido
    ahora cuando se creen vista de hanlebars o componentes se podran utilizar

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ const ahoraChile = moment().tz('America/Santiago').format('YYYY-MM-DD HH:mm:ss')
 const ahoraUTC = moment().utc().format();
 
 
+app.engine('.hbs', engine({
+    extname: '.hbs',              // extensión de los archivos
+    defaultLayout: 'main',        // layout principal
+    layoutsDir: path.join(__dirname, 'views/layouts'), // carpeta de layouts
+    partialsDir: path.join(__dirname, 'views/partials') // carpeta de parciales
+}));
+
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
+
 
 console.log('Hora UTC:', ahoraUTC);
 console.log('Hora en Chile;', ahoraChile);
