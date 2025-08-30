4dba000959e942880bf20c1f168fbc4b868e5eca
D4rK14 <mastahoscuro@gmail.com>
2025-08-29 22:30:08 -0400

    libreria de ajuste de zona horaria
    de instalo el modulo de moment-timezone para controlar los cambios de hora en verano/invierno de Chile

--- app.js
+++ app.js
@@ -2,4 +2,14 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
 
+const moment = require('moment-timezone');
+
 const app = express();
+
+const ahoraChile = moment().tz('America/Santiago').format('YYYY-MM-DD HH:mm:ss');
+
+console.log('Hora en Chile;', ahoraChile);
+
+const ahoraUTC = moment().utc().format();
+
+console.log('HOra UTC:', ahoraUTC);
