58b1fa35dff80cceb20cd7f4c0721d2e0754ab26
D4rK14 <mastahoscuro@gmail.com>
2025-09-17 01:14:46 -0300

    database en la nube
    azure

--- config/database.js
+++ config/database.js
@@ -1,18 +1,23 @@
-// Configuración de Sequelize para conectarse a MySQL
 const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-// Creamos la instancia de conexión
 const sequelize = new Sequelize(
-  process.env.DB_NAME, // Nombre de la BD
-  process.env.DB_USER, // Usuario
-  process.env.DB_PASS, // Contraseña
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
   {
-    host: process.env.DB_HOST, // Host de la BD
-    dialect: 'mysql',          // Motor de BD
-    logging: false             // Si lo pones true, verás cada query SQL en consola
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT,
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // Puedes ponerlo en true si tienes el certificado válido
+      }
+    }
   }
 );
 
