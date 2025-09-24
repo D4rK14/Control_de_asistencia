4a0237da18360ce1098697ad62b13bace7ec0743
lil.mati <matias.murasegovia@gmail.com>
2025-09-24 20:58:58 -0300

    Generar pdf de reportes y QR login

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -8,4 +8,7 @@ const reportController = require('../controllers/reportController');
 // Ruta para ver los reportes de inasistencia
 router.get('/absence', verifyToken, authorizeRole(['administrador', 'RR.HH']), reportController.getAbsenceReports);
 
+// Ruta para exportar los reportes visibles a PDF (respeta el query param filterType)
+router.get('/absence/export', verifyToken, authorizeRole(['administrador', 'RR.HH']), reportController.exportAbsenceReportsPdf);
+
 module.exports = router;
